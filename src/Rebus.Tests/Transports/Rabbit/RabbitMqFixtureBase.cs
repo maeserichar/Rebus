05301b5d968b653d0cee6d34d55ba7f56e3de748
Ricardo Rodriguez <rodriguezfernandezricardo@gmail.com>
2014-11-14 13:16:38 +0100

    Rever accidental change in RabbitMqFixtureBase

--- src/Rebus.Tests/Transports/Rabbit/RabbitMqFixtureBase.cs
+++ src/Rebus.Tests/Transports/Rabbit/RabbitMqFixtureBase.cs
@@ -13,7 +13,7 @@ namespace Rebus.Tests.Transports.Rabbit
 {
     public abstract class RabbitMqFixtureBase : IDetermineMessageOwnership
     {
-        public const string ConnectionString = "amqp://guest:guest@db.herma-ricardo.dev";
+        public const string ConnectionString = "amqp://guest:guest@localhost";
 
         protected readonly List<string> queuesToDelete = new List<string>();
 
